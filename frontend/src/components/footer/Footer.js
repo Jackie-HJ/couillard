b74364514bca25f8f1fe8924c95085cf8b4ead28
urmishukla <urmi.k.shukla@gmail.com>
2023-09-30 15:41:52 -0500

    add couillard specific design changes

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <div class="footer">
-            <p>Copyright © Urmi Shukla 2023</p>
+            <p>Copyright © DSSD-Madison 2023</p>
         </div>
     );
 }
