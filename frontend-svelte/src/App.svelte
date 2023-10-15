22788725b0c56a078b23e8524e8042623a3aee40
urmishukla <urmi.k.shukla@gmail.com>
2023-10-15 15:29:43 -0500

    setup svelte app

--- frontend-svelte/src/App.svelte
+++ frontend-svelte/src/App.svelte
@@ -2,9 +2,15 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import Nav from "./components/nav/Nav.svelte"
+  import Home from "./components/home/Home.svelte"
+  import Footer from "./components/footer/Footer.svelte"
+
 </script>
 
 <main>
+  <Nav />
+  <Home />
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
@@ -26,22 +32,9 @@
   <p class="read-the-docs">
     Click on the Vite and Svelte logos to learn more
   </p>
+  <Footer />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
---
+++ frontend-svelte/src/components/nav/Nav.svelte
@@ -0,0 +1,14 @@
+
+
+
+<div class="header">
+    <div class="width-define">
+        <div class="link-row">
+            <div class="logo">
+                Couillard Solar Foundation
+            </div>
+
+        </div>
+        
+    </div>
+</div>
