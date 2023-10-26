e53bebebd868b35b2b6bbb037a1831a65054b5ec
urmishukla <urmi.k.shukla@gmail.com>
2023-10-25 22:21:28 -0500

    edited styling to meet spec

---
+++ frontend-svelte/src/components/graph/Graph.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from 'svelte';
+  import Plotly from 'plotly.js-dist';
+
+  let container;
+
+  onMount(() => {
+    const data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16]
+    }];
+
+    const layout = {
+      title: 'Sample Graph',
+      xaxis: {
+        title: 'X Axis'
+      },
+      yaxis: {
+        title: 'Y Axis'
+      }
+    };
+
+    Plotly.newPlot(container, data, layout);
+  });
+</script>
+  
+<div bind:this={container}></div>
+  
