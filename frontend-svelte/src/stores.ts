c4504d5e071adf46fb56b397b4ec5d9e994f437e
spin-ice <tacos12368@tutanota.com>
2023-11-02 00:11:15 -0500

    whitelist thingy

--- frontend-svelte/src/stores.ts
+++ frontend-svelte/src/stores.ts
@@ -8,9 +8,16 @@ export const dbData = readable(await getData());
 const LBS_CO2_PER_KWH = 1.52;
 const DOLLARS_SAVED_PER_KWH = 0.10;
 
+const TOTALS_PANEL_WHITELIST = ["Kelly Lane"];
+
 function calcTotal(data: { [key: string]: PanelData }) {
     let total = 0;
-    for (const [_panelName, xy] of Object.entries(data)) {
+    for (const [panelName, xy] of Object.entries(data)) {
+        if (TOTALS_PANEL_WHITELIST !== null) {
+            if (!TOTALS_PANEL_WHITELIST.includes(panelName)) {
+                continue;
+            }
+        }
         for (const output of xy.y) {
             total += output;
         }
