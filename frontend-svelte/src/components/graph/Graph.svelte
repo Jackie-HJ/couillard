e132ae28ce836fb2f3283cf6d72895ccaf7fe1c1
Shivam Ratnani <ratnani@wisc.edu>
2023-11-16 05:24:03 -0600

    Backup

--- frontend-svelte/src/components/graph/Graph.svelte
+++ frontend-svelte/src/components/graph/Graph.svelte
@@ -1,47 +1,77 @@
 <script>
   import { onMount } from 'svelte';
+  // @ts-ignore
   import Plotly from 'plotly.js-dist';
 
   let container;
+  let currentData;
+
+  // Define your original data and modified data
+  const kwhData = {
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16],
+    name: 'kWh Produced',
+    type: 'scatter',
+    mode: 'lines+markers'
+  };
+
+  const lbsCO2Data = {
+    x: kwhData.x,
+    y: kwhData.y.map(y => y * 0.954),
+    name: 'lbs CO2 Saved',
+    type: 'scatter',
+    mode: 'lines+markers'
+  };
+
+  const dollarData = {
+    x: kwhData.x,
+    y: kwhData.y.map(y => y * 0.14),
+    name: 'Dollars Saved',
+    type: 'scatter',
+    mode: 'lines+markers'
+  };
+
+  const config = {
+    responsive: true,
+    displaylogo: false,
+    modeBarButtonsToRemove: ["autoScale2d", "select2d", "zoom2d", "lasso2d", "toImage", "pan2d"],
+    displayModeBar: true,
+    scrollZoom: true,
+  };
+
+  // Function to update the data and layout based on button clicked
+  function showData(selectedData) {
+    currentData = [...selectedData]; // Use spread operator to trigger reactivity
+    console.log(selectedData); // For debugging
+    let yAxisTitle = selectedData.length === 1 ? selectedData[0].name : "kWh Produced, lbs CO2 Saved, & Dollars Saved";
+
+    const updatedLayout = {
+      xaxis: { title: "Date" },
+      yaxis: { title: yAxisTitle, fixedrange: true },
+      title: "Solar Array Output",
+      dragmode: "pan"
+    };
+
+    //@ts-ignore
+    Plotly.purge(container);
+    //@ts-ignore
+    Plotly.newPlot(container, currentData, updatedLayout, config);
+  }
+
+  function resetGraph() {
+    showData([kwhData, lbsCO2Data, dollarData]);
+  }
 
   onMount(() => {
-    const data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16]
-    }];
-
-    const layout = {
-      xaxis: {
-        title: "Date",
-        //range: [new Date(new Date().getTime() - 15768000), new Date()]   - last 6 months, can use this once we have data with dates
-      },
-      yaxis: {
-        title: "Energy (kWh)",
-        fixedrange: true,
-      },
-      title: "Solar Array Output",
-      dragmode: "pan",
-    };
-
-    const config = {
-      responsive: true,
-      displaylogo: false,
-      modeBarButtonsToRemove: [
-        "autoScale2d",
-        "select2d",
-        "zoom2d",
-        "lasso2d",
-        "toImage",
-        "pan2d",
-      ],
-
-      displayModeBar: true,
-      scrollZoom: true,
-    };
-    //@ts-ignore
-    Plotly.newPlot(container, data, layout, config);
+    // Initially show all data
+    resetGraph();
   });
 </script>
-  
+
 <div bind:this={container}></div>
-  
+<div class="buttons">
+  <button on:click={() => showData([kwhData])}>Show kWh Produced</button>
+  <button on:click={() => showData([lbsCO2Data])}>Show lbs CO2 Saved</button>
+  <button on:click={() => showData([dollarData])}>Show Dollars Saved</button>
+  <button on:click={() => resetGraph()}>Show All Data</button>
+</div>
