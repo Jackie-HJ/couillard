3600d9ae28167dbc1bf0d0728fd40fe1b0cc8778
spin-ice <tacos12368@tutanota.com>
2023-11-30 15:50:32 -0600

    honestly thats just nice

--- frontend-svelte/src/animationTimings.ts
+++ frontend-svelte/src/animationTimings.ts
@@ -1,5 +1,5 @@
-export const ANIMATE_DURATION: Milliseconds = 400;
+export const ANIMATE_DURATION: Milliseconds = 300;
 
-export const ANIMATE_OUT_LOADING_TIME: Milliseconds = Math.floor((3 / 4) * ANIMATE_DURATION);
+export const ANIMATE_OUT_LOADING_TIME: Milliseconds = Math.floor((1 / 2) * ANIMATE_DURATION);
 
 type Milliseconds = number;
